7f5a2cb64876ec6c459b17f225f7f929d08d0646
Chayanin <arm.crazyplay@gmail.com>
2023-08-03 09:49:32 +0700

    first commit

---
+++ src/components/Controller.jsx
@@ -0,0 +1,9 @@
+export default function Controller() {
+  return (
+    <div className="d-flex justify-content-center gap-2">
+      <button className="btn btn-secondary">light</button>
+      <button className="btn btn-dark">dark</button>
+      <button className="btn btn-primary">toggle theme</button>
+    </div>
+  );
+}
